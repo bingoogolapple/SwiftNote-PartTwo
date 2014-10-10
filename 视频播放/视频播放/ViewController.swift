5f4cc97b2f57277e4829f4593c4ebce74b0fe769
bingoogolapple <bingoogolapple@gmail.com>
2014-10-10 23:05:40 +0800

    添加视频播放（未完成）

---
+++ 视频播放/视频播放/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  视频播放
+//
+//  Created by bingoogol on 14/10/10.
+//  Copyright (c) 2014年 bingoogol. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+class ViewController: UIViewController {
+    var moviePlayer : MPMoviePlayerController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        playVideo()
+    }
+    
+    func playVideo() {
+        var url = NSBundle.mainBundle().URLForResource("video", withExtension: "mp4")
+        
+        moviePlayer = MPMoviePlayerController(contentURL: url)
+        // 播放器的大小 16:9
+        moviePlayer.view.frame = CGRectMake(10, 20, 300, 400)
+        self.view.addSubview(moviePlayer.view)
+        moviePlayer.play()
+    }
+
+}
