fef66521ac05ad5e106d3a1dc78ee418e4159689
bingoogolapple <bingoogolapple@gmail.com>
2014-10-18 21:04:38 +0800

    完成kvo

--- KVC/KVC/ViewController.swift
+++ KVC/KVC/ViewController.swift
@@ -7,13 +7,42 @@
 //
 
 import UIKit
+/**
+KVC Key value Coding 键值编码
 
+
+KVO Key value Observer
+*/
 class ViewController: UIViewController {
+    
+    var operson:Person!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        kvcDemo2()
+        kvoDemo1()
+    }
+    
+    func kvoDemo1() {
+        // 不能观察已经释放掉的对象，如果要观察，需要是强引用对象，或者被其他对象强引用的对象
+        operson = Person()
+        operson.name = "mary"
+        // 用户姓名改变时，需要及时的到通知
+        // 如果要监听对象属性的变化，除了系统的通知中心之外，我们可以自己注册观察者
+        // 注意：观察者模式的性能不好，使用完毕后，一定记住释放观察者
         
+        // 1.观察者对象，可以是自定义对象，也可以是self
+        // 2.观察对象的键值路径
+        // 3.观察数值变化情况选项（只观察新变化的值，变化之前的数值）
+        // 4.上下文，指定观察者的同时，可以通过context设置字符串参数提示
+        operson.addObserver(self, forKeyPath: "name", options: NSKeyValueObservingOptions.New | NSKeyValueObservingOptions.Old, context: nil)
+        //点语法不会被观察到
+        //operson.name = "mike"
+        operson.setValue("mike", forKey: "name")
+        operson.removeObserver(self, forKeyPath: "name")
+    }
+    
+    override func observeValueForKeyPath(keyPath: String!, ofObject object: AnyObject!, change: [NSObject : AnyObject]!, context: UnsafeMutablePointer<Void>) {
+        println("\(keyPath)  \n \(object)  \n \(change)  \n \(context)")
     }
     
     func kvcDemo2() {
